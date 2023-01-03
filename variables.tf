ea6f60a46cf57f69b821655a507b2a96cd0adf9b
Oleksii Yesin <leha.esin.95@gmail.com>
2023-01-03 15:09:08 +0200

    wip

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "vpc_id" {
+  description = "The id of the vpc"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "A list of subnet ids to use"
+  type        = list(string)
+}
+
+variable "instance_name" {
+  description = "Instance name prefix"
+  type        = string
+  default     = "test-5"
+}
+
+variable "instance_count" {
+  description = "Number of instances to create"
+  type        = number
+  default     = 1
+}
+
+variable "extra_tags" {
+  description = "Additional tags"
+  type        = map(string)
+  default     = {}
+}
+
+variable "extra_environment" {
+  description = "List of additional environment variables"
+  type = list(object({
+    name  = string
+    value = string
+  }))
+  default = []
+}
